d00ecdc31ebae1cf1893a3933161869c44b5d66c
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-03-25 10:30:35 +0100

    ok

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -5,30 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView myTextView;
-    Button myButton;
-    Boolean hd=false;
+    LinearLayout layout_one;
+    LinearLayout two;
+    LinearLayout layout_three;
+    LinearLayout layout_four;
+    Button button_login;
+    Button button_logout;
+    Button button_singup;
+    EditText edit_login;
+    EditText edit_password;
+    TextView text_state;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myTextView=findViewById(R.id.MyTextView);
-        myButton=findViewById(R.id.MyButton);
-        myButton.setOnClickListener(this);
+         layout_one=findViewById(R.id.layout_one);
+         /* de meme pour
+         layout_two=;
+         layout_three;
+         layout_four;
+         edit_login;
+         edit_password;
+         text_state;
+        */
+
+        button_login=findViewById(R.id.button_login);
+        button_logout=findViewById(R.id.button_logout);
+        button_singup=findViewById(R.id.button_singup);
+
+        button_login.setOnClickListener(this);
+        button_logout.setOnClickListener(this);
+        button_singup.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
-        if (view.getId()==R.id.MyButton && hd) {
-            myTextView.setVisibility(View.VISIBLE);
-            hd = false;
+
+        if (view.getId()==R.id.button_login){
+
         }
-        else if (view.getId()==R.id.MyButton && !hd){
-          myTextView.setVisibility(View.INVISIBLE);
-        hd=true;}
+        else if (view.getId()==R.id.button_logout){
+
+        }
+        else if (view.getId()==R.id.button_singup){
+
+        }
     }
 }
--- app/src/main/java/com/example/firstapp/model/CR.java
+++ app/src/main/java/com/example/firstapp/model/CR.java
@@ -7,5 +7,27 @@ public class CR {
     String description;
     LinkedList<Etudiant> liste_absents;
 
+    public String getHeure_debut() {
+        return heure_debut;
+    }
 
+    public void setHeure_debut(String heure_debut) {
+        this.heure_debut = heure_debut;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LinkedList<Etudiant> getListe_absents() {
+        return liste_absents;
+    }
+
+    public void setListe_absents(LinkedList<Etudiant> liste_absents) {
+        this.liste_absents = liste_absents;
+    }
 }
